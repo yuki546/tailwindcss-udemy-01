e8c1eaf8b7300642356cec2420c2d684514e30bb
Yuki <diesunabe@gmail.com>
2024-07-02 23:32:22 +0900

    navigation-bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          100: "#ebf8ff",
-          300: "#90cdf4",
-          500: "#4299e1",
-        }
-      }
+        'body': '#050505',
+        'selected-text': '#A3A3FF',
+        'theme': '#5c318c',
+      },
     },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    }
   },
   plugins: [],
 }
