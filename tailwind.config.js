8d73c82864fdeee289b9513a1f7d09bb4ec28918
Yuki <diesunabe@gmail.com>
2024-07-07 23:02:03 +0900

    skills and contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         'selected-text': '#A3A3FF',
         theme: '#5c318c',
         secondary: '#9191A4',
+        badge: '#3F3F51',
+        inputBorder: '#565666',
+        input: '#2A2A33',
       },
     },
     fontFamily: {
